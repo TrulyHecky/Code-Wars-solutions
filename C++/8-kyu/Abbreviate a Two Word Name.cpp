ee752a05ab14ab52710b98f2c7c1d5fcde37e6e9
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-11-13 16:39:18 +0600

    Added new solution.

---
+++ C++/8-kyu/Abbreviate a Two Word Name.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+std::string abbrevName (std::string name)
+{
+  std::string ret;
+  ret.push_back(toupper(name[0]));
+  ret.push_back('.');
+  ret.push_back(toupper(name[name.find(' ') + 1]));
+  return ret;
+}
