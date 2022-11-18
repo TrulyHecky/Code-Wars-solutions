1262a210cc92231da7ad5ac9458a24148a54eee9
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-11-18 12:14:30 +0600

    Added new solution.

---
+++ Python/6-kyu/Build Tower.py
@@ -0,0 +1,8 @@
+def tower_builder (n_floor):
+    result = []
+    width = (n_floor * 2) - 1
+    for x in range(1, 2 * n_floor, 2):
+        stars = x * '*'
+        line = stars.center(width)
+        result.append(line)
+    return result
