a79daa0c7ece64f16839a8bbe72a961280cbd831
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-11-23 12:19:36 +0600

    Added new solution.

---
+++ Python/7-kyu/Jaden Casing Strings.py
@@ -0,0 +1,6 @@
+def to_jaden_case (string):
+    textArr = string.split(" ")
+    jadenCaseArr = []
+    for word in textArr:
+        jadenCaseArr.append(word.capitalize())
+    return " ".join(jadenCaseArr)
