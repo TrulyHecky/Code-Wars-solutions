68ad68a91e86f31152b5ae157cc94da17c7a926f
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-12-09 13:50:59 +0600

    Updated.

--- C++/8-kyu/1. Abbreviate a Two Word Name.cpp
+++ C++/8-kyu/1. Abbreviate a Two Word Name.cpp
@@ -5,6 +5,6 @@ std::string abbrevName (std::string name)
   std::string ret;
   ret.push_back(toupper(name[0]));
   ret.push_back('.');
-  ret.push_back(toupper(name[name.find(' ') + 1]))
+  ret.push_back(toupper(name[name.find(' ') + 1]));
   return ret;
 }
